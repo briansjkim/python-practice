69314ace56eee4f058bb4003cb9ec31c43ac7153
Brian Kim <briaansjkim@gmail.com>
2020-06-29 15:51:21 -0400

    practice w/ casefold method

---
+++ stringMethods.py
@@ -0,0 +1,3 @@
+# casefold() returns a string where all the characters are lower case
+# it's similar to the lower() method, but casefold() will convert more characters into lower case
+print('Brian KIM'.casefold())
