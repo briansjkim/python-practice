1606bc091227a4fa49d51ec0657f91c03f34bbc5
Brian Kim <briaansjkim@gmail.com>
2020-06-29 16:07:50 -0400

    practice w/ split method

--- stringMethods.py
+++ stringMethods.py
@@ -16,3 +16,15 @@ print('Does your {} {}?'.format(animal, verb))
 
 # title() method returns a string in title case meaning the first letter of each word is capitalized
 print('the man in the woods said hi'.title())
+
+# split() methods returns a container called a list that contains the words from the input string
+# has two additional arguments (sep and maxsplit)
+    # the sep argument stands for separator which can be used to identify how the string should be split (splice, tab, return, new line, comma, etc.).
+        # if the sep argument isn't provided, the default separator is whitespace
+        # the sep argument CANNOT be empty if used with the maxsplit argument. You can use None as the separator if using the maxsplit argument
+    # the maxsplit argument provides the number of maximum splits. the argument gives maxsplit + 1 number of elements in the new list with the remaining string being returned as the last element in the list
+
+my_string = 'The cow goes moo over the moon'
+print(my_string.split(' ', 3)) # ['The', 'cow', 'goes', 'moo over the moon']
+print(my_string.split(None, 2)) # ['The', 'cow', 'goes moo over the moon']
+print(my_string.split(' ', 4)) # ['The', 'cow', 'goes', 'moo', 'over the moon']
