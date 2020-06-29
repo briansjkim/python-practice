48e376a31af0d7aa0be95f5dca2f76856a5f443b
Brian Kim <briaansjkim@gmail.com>
2020-06-29 16:31:34 -0400

    init list

---
+++ dataStructures/list.py
@@ -0,0 +1,7 @@
+# lists are containers that organize and group data types together
+    # A list is a data structure that is a mutable ordered sequence of elements
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+print(days[0]) # Monday
+print(days[10]) # IndexError: list index out of range
+
