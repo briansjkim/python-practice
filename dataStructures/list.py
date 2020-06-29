d0d2b8c21ab846e7ee9076ad8eec6fd8d9a2115b
Brian Kim <briaansjkim@gmail.com>
2020-06-29 16:34:13 -0400

    practice w/ list

--- dataStructures/list.py
+++ dataStructures/list.py
@@ -1,7 +1,8 @@
 # lists are containers that organize and group data types together
     # A list is a data structure that is a mutable ordered sequence of elements
 days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+randoms = ['hello', 1, 4, 5.8, False]
 
 print(days[0]) # Monday
-print(days[10]) # IndexError: list index out of range
-
+# print(days[10]) # IndexError: list index out of range
+print(randoms[3]) # 5.8
