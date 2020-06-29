cd64de9ffbb2bb91c624f0718822a4a66b637cd6
Brian Kim <briaansjkim@gmail.com>
2020-06-29 15:56:19 -0400

    practice with format method

--- stringMethods.py
+++ stringMethods.py
@@ -4,3 +4,12 @@ print('Brian KIM'.casefold())
 
 # lower() is very similar to casefold() where it returns a string with all the characters being lower case
 print('BrIAN KIM'.lower())
+
+# format() method formats the specified value(s) and insert them inside the string's placeholder
+print('Mohammed has {} balloons'.format(27))
+print('Does your {} {}?'.format('dog', 'bite'))
+
+animal = 'bat'
+verb = 'bite'
+
+print('Does your {} {}?'.format(animal, verb))
