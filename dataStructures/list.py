5646972675c3691d569f897661ec7c06d42eb76d
Brian Kim <briaansjkim@gmail.com>
2020-06-29 18:42:48 -0400

    practice w/ slicing

--- dataStructures/list.py
+++ dataStructures/list.py
@@ -26,3 +26,8 @@ print('Monday' in days) # True
 print('Monday' in weekends) # False
 print('January' not in days) # True
 print(weekends not in randoms) # True
+
+# Slicing Quiz
+# Select the three most recent dates from this list
+eclipse_dates = ['June 21, 2001', 'December 4, 2002', 'November 23, 2003', 'March 29, 2006', 'August 1, 2008', 'July 22, 2009', 'July 11, 2010', 'November 13, 2012', 'March 20, 2015','March 9, 2016']
+print(eclipse_dates[-3: ])
