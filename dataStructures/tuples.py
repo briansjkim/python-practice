2ad11920dc849da0dad000463bb23893e2d9d450
Brian Kim <briaansjkim@gmail.com>
2020-06-30 16:23:29 -0400

    tuple example

--- dataStructures/tuples.py
+++ dataStructures/tuples.py
@@ -1,3 +1,6 @@
 # Tuples are another useful container
 # A data type for immutable ordered sequences of elements that are often used to store related pieces of info
 
+location = (13.4125, 103.86667)
+print("Latitude: ", location[0])
+print("longitude: ", location[1])
