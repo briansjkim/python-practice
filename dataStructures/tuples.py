9c2fc8bbdc4284ce7a72384e67850211a436dcfb
Brian Kim <briaansjkim@gmail.com>
2020-06-30 16:25:51 -0400

    more practice w/ tuples

--- dataStructures/tuples.py
+++ dataStructures/tuples.py
@@ -4,3 +4,11 @@
 location = (13.4125, 103.86667)
 print("Latitude: ", location[0])
 print("longitude: ", location[1])
+
+# Tuples are similar to lists in that they store an ordered collection of objects which can be accessed by their indices.
+# However, tuples are immutable so you can't add and remove items from tuples or sort them in place
+# Can also be used to assign multiple variables in a compact way
+
+dimensions = 52, 40, 100
+length, width, height = dimensions
+print('The dimensions are {} x {} x {}'.format(length, width, height))
