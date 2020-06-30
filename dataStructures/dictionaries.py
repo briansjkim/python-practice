ec6122137860fa7ab735fe4777e4cab32acbc6e6
Brian Kim <briaansjkim@gmail.com>
2020-06-30 17:44:06 -0400

    init dictionary

---
+++ dataStructures/dictionaries.py
@@ -0,0 +1,8 @@
+# Dictionaries are mutable data types that stores mappings of unique keys to values
+
+# You can look up values, change values, or insert new values in the dictionary using square brackets that enclose the key
+elements = {'hydrogen': 1, 'helium': 2, 'carbon': 6}
+print(elements['helium'])
+elements['lithium'] = 3
+elements['hydrogen'] = 4
+print(elements)
