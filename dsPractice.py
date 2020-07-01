f603e8559c18a750102c00bdd360b63650516516
Brian Kim <briaansjkim@gmail.com>
2020-07-01 14:05:21 -0400

    init problems

--- dsPractice.py
+++ dsPractice.py
@@ -15,3 +15,12 @@ verse_set = set(verse_list)
 # print the number of unique words
 # Subtract one from the length because an empty space will be inside the set
 num_unique = len(verse_set) - 1
+
+
+verse_dict = {'if': 3, 'you': 6, 'can': 3, 'keep': 1, 'your': 1, 'head': 1, 'when': 2, 'all': 2, 'about': 2, 'are': 1, 'losing': 1, 'theirs': 1, 'and': 3, 'blaming': 1, 'it': 1, 'on': 1, 'trust': 1, 'yourself': 1, 'men': 1, 'doubt': 1, 'but': 1, 'make': 1, 'allowance': 1, 'for': 1, 'their': 1, 'doubting': 1, 'too': 3, 'wait': 1, 'not': 1, 'be': 1, 'tired': 1, 'by': 1, 'waiting': 1, 'or': 2, 'being': 2, 'lied': 1, 'don\'t': 3, 'deal': 1, 'in': 1, 'lies': 1, 'hated': 1, 'give': 1, 'way': 1, 'to': 1, 'hating': 1, 'yet': 1, 'look': 1, 'good': 1, 'nor': 1, 'talk': 1, 'wise': 1}
+
+# 1. How many unique words are in verse_dict?
+# 2. Is the key "breathe" in verse_dict?
+# 3. What is the first element in the list created when verse_dict is sorted by keys?
+    # Use the appropriate dictionary method to get a list of its keys, and then sort that list
+# 4. Which key (word) has the highest value in verse_dict?
