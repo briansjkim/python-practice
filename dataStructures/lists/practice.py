692bd037fca599fcc576b8a991d367c7a24d86af
Brian Kim <briaansjkim@gmail.com>
2020-07-01 15:16:34 -0400

    completed first exercise

--- dataStructures/lists/practice.py
+++ dataStructures/lists/practice.py
@@ -1,2 +1,10 @@
 # Python List Exercises
 
+# Given a Python list you should be able to display Python list in the following order:
+#  [100, 200, 300, 400, 500] => [500, 400, 300, 200, 100]
+
+aList = [100, 200, 300, 400, 500]
+
+# this is a slicing technique used to produce a reversed copy
+newList = aList[::-1]
+print(newList)
