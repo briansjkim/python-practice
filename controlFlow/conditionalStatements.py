771207544bbca3200cea590cb8c1e9003b89d116
Brian Kim <briaansjkim@gmail.com>
2020-07-01 15:40:30 -0400

    init exercise

---
+++ controlFlow/conditionalStatements.py
@@ -0,0 +1,9 @@
+# Write an if statement that lets a competitor know which of these prizes they won based on the number of points they scored, which is stored in the integer variable points
+
+# Points   Prize
+# ---------------
+# 1-50     wooden rabbit
+# 51-150   no prize
+# 151-180  wafer-thin mint
+# 181-200  penguin
+
