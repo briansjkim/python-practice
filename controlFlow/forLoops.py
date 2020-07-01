b5e5b107c028d3edb6aec66cfcf6b059d5d32d81
Brian Kim <briaansjkim@gmail.com>
2020-07-01 17:38:19 -0400

    practice w/ range function

--- controlFlow/forLoops.py
+++ controlFlow/forLoops.py
@@ -5,5 +5,16 @@ for city in cities:
 print('Completed')
 
 # Using range() function
+# range(start = 0, stop, step = 1)
+
+# only one argument => stop
 for i in range(3):
-    print('Hello')
+    print(i) #0, 1, 2
+
+# using two arguments => start and stop
+for i in range(2, 6):
+    print(i) #2, 3, 4, 5
+
+# using three arguments => start, stop, and step
+for i in range(1, 10, 2):
+    print(i) # 1, 3, 5, 7, 9
