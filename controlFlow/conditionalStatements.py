7c56ad5fa790d8cebac1729948f49ac250d17ff5
Brian Kim <briaansjkim@gmail.com>
2020-07-01 17:27:28 -0400

    completed conditional statements

--- controlFlow/conditionalStatements.py
+++ controlFlow/conditionalStatements.py
@@ -19,3 +19,22 @@ else:
     result = 'Congratulations! You won a penguin!'
 
 print(result)
+
+# Using complex conditional statements
+points = 174
+
+prize = None
+
+if points <= 50:
+    prize = 'wood rabbit'
+elif 150 <= points <= 180:
+    prize = 'wafer-thin mint'
+elif points >= 181:
+    prize = "penguin"
+
+if prize:
+    result = 'Congratulations! You have won a {}!'.format(prize)
+else:
+    result = 'Oh dear, no prize this time.'
+
+print(result)
