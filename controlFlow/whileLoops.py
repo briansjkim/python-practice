9a1174aeabce9ddb450bfdecb0d84ac57ce22a76
Brian Kim <briaansjkim@gmail.com>
2020-07-02 17:39:29 -0400

    example of while loop

--- controlFlow/whileLoops.py
+++ controlFlow/whileLoops.py
@@ -1,2 +1,9 @@
 # While loops are an example of "indefinite iteration" which is when a loop repeats an unknown number of times and ends when some condition is met
 
+card_deck = [4, 11, 8, 5, 13, 2, 8, 10]
+hand = []
+
+while sum(hand) < 17:
+    hand.append(card_deck.pop())
+
+print(hand)
