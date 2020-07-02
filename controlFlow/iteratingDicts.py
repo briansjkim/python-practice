2aed78bab94796c611db9a5abedb027ceab79fed
Brian Kim <briaansjkim@gmail.com>
2020-07-02 17:12:10 -0400

    first exercise

--- controlFlow/iteratingDicts.py
+++ controlFlow/iteratingDicts.py
@@ -1,2 +1,12 @@
 # Practice w/ iterating through dictionaries with a for loop
 
+# To access the keys, you iterate through a dict with a regular for loop
+cast = {
+    'Jerry Seinfeld': 'Jerry Seinfeld',
+    'Julia Louis-Dreyfus': 'Elaine Benes',
+    'Jason Alexander': 'George Costanza',
+    'Michael Richards': 'Cosmo Kramer'
+}
+
+for key in cast:
+    print(key)
