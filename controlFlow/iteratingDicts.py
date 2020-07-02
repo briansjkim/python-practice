aef3e27c2bdfe0b4cbfd0bfeda72df25262fed09
Brian Kim <briaansjkim@gmail.com>
2020-07-02 17:37:20 -0400

    completed iterating through dicts

--- controlFlow/iteratingDicts.py
+++ controlFlow/iteratingDicts.py
@@ -17,4 +17,12 @@ for key, val in cast.items():
 
 # Task 1
 # Count the number of fruits in your basket. Given a dictionary and list of fruits. If the key is in the list of fruits, add the value (number of fruits) to result
+result = 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
 
+for item, count in basket_items.items():
+    if item in fruits:
+        result += count
+
+print(result)
